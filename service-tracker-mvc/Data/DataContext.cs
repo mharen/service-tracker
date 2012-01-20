7b9bd01bd4842afd78e9a3e5c1b4f0525db70c69
Michael Haren <mharen@gmail.com>
2012-01-20 15:30:30 -0500

    I'm bad at this

---
+++ service-tracker-mvc/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using service_tracker_mvc.Models;
+
+namespace service_tracker_mvc.Data
+{
+    public class DataContext :DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Servicer> Servicers { get; set; }
+    }
+}
