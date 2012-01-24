cad755ba7d1f72d0033d6cb46535e1a295083880
Michael Haren <mharen@gmail.com>
2012-01-24 17:09:45 -0500

    and these...

---
+++ service-tracker-mvc/Models/InvoiceFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace service_tracker_mvc.Models
+{
+    [NotMapped]
+    public class InvoiceFilter
+    {
+        [DisplayName("Service Date Range")]
+        public DateTime? StartDate { get; set; }
+        [DisplayName("Service Date Range")]
+        public DateTime? EndDate { get; set; }
+        [DisplayName("Employee")]
+        public int ServicerId { get; set; }
+        [DisplayName("Customer")]
+        public int CustomerId { get; set; }
+        [DisplayName("Key Rec")]
+        public string KeyRec { get; set; }
+        [DisplayName("FRT Bill")]
+        public string FrtBill { get; set; }
+        [DisplayName("Purchase Order")]
+        public string PurchaseOrder { get; set; }
+    }
+}
---
+++ service-tracker-mvc/Models/InvoiceIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace service_tracker_mvc.Models
+{
+    public class InvoiceIndexViewModel
+    {
+        public List<Invoice> Invoices { get; set; }
+        public InvoiceFilter InvoiceFilter { get; set; }
+    }
+}
