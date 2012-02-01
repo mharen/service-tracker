2a110665e84e5cebc5d237b9927aafebae4b1d9f
Michael Haren <mharen@gmail.com>
2012-02-01 11:14:15 -0500

    EF MIGRATIONS MOAR MIGRATIONS WOOHOOOOOOOO

---
+++ service-tracker-mvc/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace service_tracker_mvc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<service_tracker_mvc.Data.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(service_tracker_mvc.Data.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
