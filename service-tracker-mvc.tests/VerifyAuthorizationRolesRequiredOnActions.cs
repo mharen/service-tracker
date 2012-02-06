9ea43608fa7f0501e0b2eca3630c1c6f6716898c
Michael Haren <mharen@gmail.com>
2012-02-06 15:53:40 -0500

    oh yeah...these files, too

---
+++ service-tracker-mvc.tests/VerifyAuthorizationRolesRequiredOnActions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using service_tracker_mvc.Controllers;
+
+namespace service_tracker_mvc.tests
+{
+    [TestClass]
+    public class VerifyAuthorizationRolesRequiredOnActions
+    {
+        [TestMethod, Ignore]
+        public void VerifyNonManagersCannotAccessRestrictedAreas()
+        {
+            // arrange
+            //var Controller = new InvoiceController();
+            
+            // act
+
+            // assert
+        }
+    }
+}
