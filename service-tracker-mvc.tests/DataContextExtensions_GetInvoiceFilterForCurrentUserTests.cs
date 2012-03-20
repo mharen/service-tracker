125d3aa5bc512c3a805fe5dad13b7ba6cac7a456
Michael Haren <mharen@gmail.com>
2012-03-20 11:37:09 -0400

    organizing unit tests

--- service-tracker-mvc.tests/DataContextExtensions_GetInvoiceFilterForCurrentUserTests.cs
+++ service-tracker-mvc.tests/DataContextExtensions_GetInvoiceFilterForCurrentUserTests.cs
@@ -13,206 +13,150 @@ using service_tracker_mvc.Classes;
 
 namespace service_tracker_mvc.tests
 {
-    
-    
-    /// <summary>
-    ///This is a test class for DataContextExtensionsTest and is intended
-    ///to contain all DataContextExtensionsTest Unit Tests
-    ///</summary>
     [TestClass()]
     public class DataContextExtensions_GetInvoiceFilterForCurrentUserTests
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Administrator()
         {
-            var FakeUser = GetFakeUser(RoleType.Administrator);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Administrator);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Administrator()
         {
-            var FakeUser = GetFakeUser(RoleType.Administrator);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Administrator);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Administrator()
         {
-            var FakeUser = GetFakeUser(RoleType.Administrator);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Administrator);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Manager()
         {
-            var FakeUser = GetFakeUser(RoleType.Manager);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Manager);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Manager()
         {
-            var FakeUser = GetFakeUser(RoleType.Manager);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Manager);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Manager()
         {
-            var FakeUser = GetFakeUser(RoleType.Manager);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Manager);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Supervisor()
         {
-            var FakeUser = GetFakeUser(RoleType.Supervisor);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Supervisor);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Supervisor()
         {
-            var FakeUser = GetFakeUser(RoleType.Supervisor);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Supervisor);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Supervisor()
         {
-            var FakeUser = GetFakeUser(RoleType.Supervisor);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Supervisor);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Customer()
         {
-            var FakeUser = GetFakeUser(RoleType.Customer);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Customer);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Customer()
         {
-            var FakeUser = GetFakeUser(RoleType.Customer);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Customer);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Customer()
         {
-            var FakeUser = GetFakeUser(RoleType.Customer);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Customer);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Employee()
         {
-            var FakeUser = GetFakeUser(RoleType.Employee);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Employee);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: true);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Employee()
         {
-            var FakeUser = GetFakeUser(RoleType.Employee);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Employee);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Employee()
         {
-            var FakeUser = GetFakeUser(RoleType.Employee);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Employee);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUser_ByCurrentServicer_Guest()
         {
-            var FakeUser = GetFakeUser(RoleType.Guest);
-            var FakeInvoice = GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Guest);
+            var FakeInvoice = Statics.GetFakeInvoice(servicerId: FakeUser.ServicerId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByCurrentOrganization_Guest()
         {
-            var FakeUser = GetFakeUser(RoleType.Guest);
-            var FakeInvoice = GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
+            var FakeUser = Statics.GetFakeUser(RoleType.Guest);
+            var FakeInvoice = Statics.GetFakeInvoice(organizationId: FakeUser.OrganizationId.Value);
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
         [TestMethod()]
         public void GetInvoiceFilterForCurrentUserTest_ByNeitherCurrentOrganizationOrServicer_Guest()
         {
-            var FakeUser = GetFakeUser(RoleType.Guest);
-            var FakeInvoice = GetFakeInvoice();
+            var FakeUser = Statics.GetFakeUser(RoleType.Guest);
+            var FakeInvoice = Statics.GetFakeInvoice();
             VerifyInvoiceFilter(FakeUser, FakeInvoice, expectedResult: false);
         }
 
@@ -222,27 +166,7 @@ namespace service_tracker_mvc.tests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        private ICurrentUser GetFakeUser(RoleType roleType)
-        {
-            return new CurrentUser()
-            {
-                Role = roleType,
-                Name = "name",
-                ServicerId = 1,
-                OrganizationId = 2
-            };
-        }
-
-        private Invoice GetFakeInvoice(int servicerId = 0, int organizationId = 0){
-            return new Invoice()
-            {
-                ServicerId = servicerId,
-                Site = new Site()
-                {
-                    OrganizationId = organizationId
-                }
-            };
-        }
+        
 
         ///// <summary>
         /////A test for GetServicerFilterForCurrentUser
---
+++ service-tracker-mvc.tests/Statics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using service_tracker_mvc.Classes;
+using service_tracker_mvc.Models;
+
+namespace service_tracker_mvc.tests
+{
+    public static class Statics
+    {
+        public static ICurrentUser GetFakeUser(RoleType roleType)
+        {
+            return new CurrentUser()
+            {
+                Role = roleType,
+                Name = "name",
+                ServicerId = 1,
+                OrganizationId = 2
+            };
+        }
+
+        public static Invoice GetFakeInvoice(int servicerId = 0, int organizationId = 0)
+        {
+            return new Invoice()
+            {
+                ServicerId = servicerId,
+                Site = new Site()
+                {
+                    OrganizationId = organizationId
+                }
+            };
+        }
+    }
+}
