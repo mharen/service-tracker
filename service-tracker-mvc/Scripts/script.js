0b574dc8dff41aaade1a09d58913c8f6c6a281e3
Michael Haren <mharen@gmail.com>
2012-03-22 00:33:29 -0400

    cleaning up CDN-hosted packages

--- service-tracker-mvc/Scripts/script.js
+++ service-tracker-mvc/Scripts/script.js
@@ -1,3 +1,12 @@
+/**** Utilities ****/
+window.log = function () {
+    log.history = log.history || [];
+    log.history.push(arguments);
+    arguments.callee = arguments.callee.caller;
+    if (this.console) console.log(Array.prototype.slice.call(arguments));
+};
+(function (b) { function c() { } for (var d = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","), a; a = d.pop(); ) b[a] = b[a] || c })(window.console = window.console || {});
+
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -5,40 +14,49 @@ function formatMoney(n) {
     return (isNumber(n) ? n : 0).toFixed(2);
 }
 
+/**** Every page load ****/
 $(function () {
 
-    // patch the validate "date" method to accomodate iOS-style ISO dates
-    // because somehow iOS supports HTML5 date inputs, but its Date() implementation
-    // doesn't parse them... WUT?!
-    var originalDateValidator1 = $.validator.methods.date;
-    var originalDateValidator2 = $.validator.methods.dateISO;
-    $.validator.methods.date = function (value, element) {
-        var isValidDate =
+    if ($.validator) {
+        // patch the validate "date" method to accomodate iOS-style ISO dates
+        // because somehow iOS supports HTML5 date inputs, but its Date() implementation
+        // doesn't parse them... WUT?!
+        var originalDateValidator1 = $.validator.methods.date;
+        var originalDateValidator2 = $.validator.methods.dateISO;
+        $.validator.methods.date = function (value, element) {
+            var isValidDate =
             originalDateValidator1.apply(this, arguments) ||
             originalDateValidator2.apply(this, arguments);
-        return isValidDate;
-    };
-
-    if (!Modernizr.inputtypes.date) {
-        $("input[type=date]").datepicker().attr("novalidate", "novalidate");
+            return isValidDate;
+        };
     }
 
+    // attach the jquery datepicker unless the current browser has one
+    if (!Modernizr.inputtypes.date) {
+        $("input[type=date]").datepicker();
+    }
+
+    // prompt for confirmation on some buttons
     $("input[type='submit'].confirm-no-undo").click(function () {
         return confirm("Are you sure? There is no undo!");
     });
 
+    // add hover effects to tables
     $('table.ui-widget tr').hover(
         function () { $(this).children('td').addClass('ui-state-hover'); },
         function () { $(this).children('td').removeClass('ui-state-hover'); }
     )
+    // and click effects
     .click(function (e) {
         if ($(e.target).is('tr,td,th')) {
             $(this).children('td').toggleClass('ui-state-highlight');
         }
     });
 
+    // make the tabs look nice when hovered
     $('#menu li').hover(function () { $(this).addClass('ui-state-hover'); }, function () { $(this).removeClass('ui-state-hover'); });
 
+    // buttonize inputs and buttons to make them look all fancy
     $('input:submit, button').button();
 
     // note: if there is no message, this will silently just not do anything :)
